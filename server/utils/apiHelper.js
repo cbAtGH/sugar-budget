7ab91997157a8289efeed6b0be4686c0f67b0474
hawkess <getseriousjunk@gmail.com>
2022-07-26 08:14:51 -0500

    JSDoc comments added

--- server/utils/apiHelper.js
+++ server/utils/apiHelper.js
@@ -1,5 +1,9 @@
 import mv from "../api/mealviewer.js";
 
+/**
+ * Helper function to log errors
+ * @param {object} err - error object from calling function
+ */
 const reportError = (err) => {
   if (err.response) {
     console.log(err.response.data);
@@ -12,14 +16,19 @@ const reportError = (err) => {
   }
 };
 
+/**
+ * Make menu API request to mealviewer
+ * @param {object} query - Menu query object passed from front end. Destructured out into school, startDate, and endDate query terms
+ * @returns {object} JSON payload containing all of the date and meal information for the requested period
+ */
 const requestDailyMenuData = async ({ school, startDate, endDate }) => {
-  //TODO: Check and utilize user-specified start and end dates instead of defaulting
   const querySchool = school.split(" ").join("");
   const parseDate = (date) => {
     return new Date(date).toLocaleDateString().split("/").join("-");
   };
   const start = startDate || parseDate(Date.now());
   const end = endDate || startDate;
+  if (end < start) [start, end] = [end, start]; // Swap dates under the assumption that end date was unintentionally before start date
   const queryDates = `${start}/${end}`;
   const url = `/school/${querySchool}/${queryDates}`;
   const { data } = await mv.get(url).catch((err) => {
@@ -29,6 +38,11 @@ const requestDailyMenuData = async ({ school, startDate, endDate }) => {
   return data;
 };
 
+/**
+ * Make location API request to mealviewer
+ * @param {object} query - Location query object passed from front end. Destructured out into location query term
+ * @returns {object} JSON payload containing information for all locations matching the query term
+ */
 const requestLocationData = async ({ location }) => {
   const url = `/physicalLocation/search/${location}`;
   const { data } = await mv.get(url).catch((err) => {
@@ -38,6 +52,11 @@ const requestLocationData = async ({ location }) => {
   return data;
 };
 
+/**
+ * Abridge meal data by culling fields unnecessary to calculating and displaying daily sugar budget adherence
+ * @param {object} data - JSON payload received from mealviewer API request for menu information
+ * @returns {object} JSON payload containing abridged meal data containing only nutrition and date information
+ */
 const transformDailyMenuData = (data) => {
   let meals = { dailyMenu: [], scheduledMeals: [] };
   const unitRgx = /([^)]+)\s\(([^)]+)\)/;
@@ -50,6 +69,7 @@ const transformDailyMenuData = (data) => {
       portionQuantity,
       portionSize,
     } of foodItemData) {
+      // Iterating over static menu items that don't change on a daily basis (milk, condiments)
       let nutritionData = Object.fromEntries(
         nutritionals.reduce((result, { name, rawValue, value }) => {
           const match = name.match(unitRgx);
@@ -82,6 +102,7 @@ const transformDailyMenuData = (data) => {
   }
 
   for (const menuSchedule of data.menuSchedules) {
+    // Iterating over rotating menu items that change on a daily basis
     let schedule = { date: menuSchedule.dateInformation, meals: [] };
     for (const { blockName, cafeteriaLineList } of menuSchedule.menuBlocks) {
       let items = extractNutritionData(
